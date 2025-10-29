5b886a0be77475936343bf2dcd8a6115b91ecaac
diepvn1 <diepvu5499@gmail.com>
2025-10-29 21:28:15 +0700

    first-commit

---
+++ src/main/java/com/example/springdemo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author diepvu5499
+ */
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
